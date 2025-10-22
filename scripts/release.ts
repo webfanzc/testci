910334f371c806c28ebf786f1abcdfd946ddec2c
skelanimals <zhangcong@9linktech.com>
2025-10-22 15:53:28 +0800

    chore: release v1.0.4

---
+++ scripts/release.ts
@@ -0,0 +1,18 @@
+import { execSync } from 'node:child_process'
+import { readFileSync } from 'node:fs'
+import process from 'node:process'
+
+const { version: oldVersion } = JSON.parse(readFileSync('package.json', { encoding: 'utf8' }))
+
+execSync('bumpp --no-commit --no-tag --no-push', { stdio: 'inherit' })
+
+const { version } = JSON.parse(readFileSync('package.json', { encoding: 'utf8' }))
+
+if (oldVersion === version) {
+  console.log('version error')
+  process.exit()
+}
+
+execSync('git add .', { stdio: 'inherit' })
+execSync(`git commit -m "chore: release v${version}"`, { stdio: 'inherit' })
+execSync(`git tag -a v${version} -m "v${version}"`, { stdio: 'inherit' })
