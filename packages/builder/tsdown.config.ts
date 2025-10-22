2f98791bd0b3f51dce1423639d43ee19c1d4ecf6
skelanimals <zhangcong@9linktech.com>
2025-10-22 14:28:32 +0800

    chore: 测试

--- packages/builder/tsdown.config.ts
+++ packages/builder/tsdown.config.ts
@@ -2,7 +2,7 @@ import { createTsdownConfig } from '../../tsdown.config'
 
 export default createTsdownConfig({
   entry: {
-    'index': './src/index.ts',
+    index: './src/index.ts',
   },
   external: ['fast-glob', 'pkg-types', 'ts-dedent'],
 })
