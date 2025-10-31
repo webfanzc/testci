0a2928cfa3e70fe7717913872a4bcf5bbdb93150
skelanimals <zhangcong@9linktech.com>
2025-10-31 10:59:27 +0800

    chore: 修改tag创建逻辑 在主分支合并pr或推送release commit时自动创建tag

--- scripts/release.ts
+++ scripts/release.ts
@@ -15,4 +15,5 @@ if (oldVersion === version) {
 
 execSync('git add .', { stdio: 'inherit' })
 execSync(`git commit -m "chore: release v${version}"`, { stdio: 'inherit' })
-execSync(`git tag -a v${version} -m "v${version}"`, { stdio: 'inherit' })
+// 移动到了publish.yml中
+// execSync(`git tag -a v${version} -m "v${version}"`, { stdio: 'inherit' })
